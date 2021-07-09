c7739ecb524c106773a1917f6e6e70585b0f65a2
Ilikedietpepsi <85099505+Ilikedietpepsi@users.noreply.github.com>
2021-07-09 10:44:36 +0800

    Create Main.java

---
+++ Easy/leetCode121/Main.java
@@ -0,0 +1,14 @@
+class Main {
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+        int max = 0;
+        int min = prices[0];
+        for(int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i]-min);
+            min = Math.min(min, prices[i]);
+        }
+        return max;      
+    }
+}
